5edef275b8434e75d01099c6a40ce524e662ae59
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-21 19:34:27 +0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -2,4 +2,5 @@ import time
 import re
 import sys
 
-os.system ("sudo python src/flaresolverr.py & sleep 3 && curl  'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{  \"cmd\": \"request.get\",  \"url\":\"https://sharemania.us/\",  \"maxTimeout\": 60000}'")
+html = os.system ("sudo python src/flaresolverr.py & sleep 3 && curl  'http://localhost:8191/v1' -H 'Content-Type: application/json' --data '{  \"cmd\": \"request.get\",  \"url\":\"https://sharemania.us/\",  \"maxTimeout\": 60000}'")
+print (html);
